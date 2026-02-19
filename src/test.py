aa7d9d0ca436f49995732021f768402e47e668e1
James McClung <james@themcclungs.net>
2026-02-19 18:32:12 -0500

    -field_array; test

--- src/test.py
+++ src/test.py
@@ -1,12 +1,10 @@
 import lib.centering
-import lib.field_array
 import lib.range3
 import lib.scalar_field
 import lib.vec3
 import lib.vector_field
 
 lib.centering.test()
-lib.field_array.test()
 lib.range3.test()
 lib.scalar_field.test()
 lib.vec3.test()
