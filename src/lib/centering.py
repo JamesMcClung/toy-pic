646158dfc378b50640bfa8aa81867ef831f1822b
James McClung <james@themcclungs.net>
2026-02-18 19:20:56 -0500

    centering: +component_centered

--- src/lib/centering.py
+++ src/lib/centering.py
@@ -1,8 +1,30 @@
 from enum import Enum, auto
 
+from lib.vec3 import Bool3
 
-class Centering(Enum):
+
+class VectorCentering(Enum):
     NC = auto()
     EC = auto()
     FC = auto()
     CC = auto()
+
+    def component_centered(self, d: int) -> Bool3:
+        match self:
+            case VectorCentering.NC:
+                return Bool3(False, False, False)
+            case VectorCentering.EC:
+                return Bool3(d == 0, d == 1, d == 2)
+            case VectorCentering.FC:
+                return Bool3(d != 0, d != 1, d != 2)
+            case VectorCentering.CC:
+                return Bool3(True, True, True)
+
+
+if __name__ == "__main__":
+    # basic tests
+
+    assert (VectorCentering.EC.component_centered(0) == Bool3(True, False, False)).all()
+    assert (VectorCentering.FC.component_centered(0) == Bool3(False, True, True)).all()
+
+    print("tests passed")
