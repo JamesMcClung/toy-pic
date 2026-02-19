2a72879f1f0e82c37a49a130e1be4139e9f9d983
James McClung <james@themcclungs.net>
2026-02-19 09:00:45 -0500

    vector_field: fix dims when periodic

--- src/lib/vector_field.py
+++ src/lib/vector_field.py
@@ -4,7 +4,7 @@ from lib.centering import VectorCentering
 from lib.domain import Domain
 from lib.field_array import FieldArray
 from lib.range3 import Range3
-from lib.vec3 import Float3, Int3
+from lib.vec3 import Bool3, Float3, Int3
 
 
 class VectorField:
@@ -18,8 +18,8 @@ class VectorField:
 
         self._components = []
         for d in range(3):
-            # nc values at upper edge are in domain
-            comp_dims = domain.dims + centering.component_centered(d).__invert__().to_mask() * domain.vary_dims.to_mask()
+            # nc values at upper edge of aperiodic dimension are in the domain
+            comp_dims = domain.dims + (~domain.periodic_dims & ~self.centering.component_centered(d)).to_mask()
             self._components.append(FieldArray(comp_dims, n_ghosts=domain.vary_dims.to_mask() * domain.n_ghosts))
 
     def set_component_from_func(self, d: int, func: Callable[[Float3], float]):
@@ -45,14 +45,14 @@ class VectorField:
 
 def test():
     dims = Int3(1, 8, 4)
-    domain = Domain(dims, Float3(0.5, 0.5, 0.5))
+    domain = Domain(dims, Float3(0.5, 0.5, 0.5), periodic_dims=Bool3(True, False, True))
     ec_field = VectorField(domain, VectorCentering.EC)
 
-    assert (ec_field.x.dims == Int3(1, 9, 5)).all()
+    assert (ec_field.x.dims == Int3(1, 9, 4)).all()
     assert (ec_field.x.n_ghosts_lower == Int3(0, 1, 1)).all()
     assert (ec_field.x.n_ghosts_upper == Int3(0, 1, 1)).all()
 
-    assert (ec_field.y.dims == Int3(1, 8, 5)).all()
+    assert (ec_field.y.dims == Int3(1, 8, 4)).all()
     assert (ec_field.y.n_ghosts_lower == Int3(0, 1, 1)).all()
     assert (ec_field.y.n_ghosts_upper == Int3(0, 1, 1)).all()
 
