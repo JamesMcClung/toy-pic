c386d468a827ec76aa8ef27c671a4106792c5ebd
James McClung <james@themcclungs.net>
2026-02-19 10:03:03 -0500

    centering: index VectorCentering

--- src/lib/centering.py
+++ src/lib/centering.py
@@ -32,14 +32,14 @@ class VectorCentering:
     def __init__(self, x: ScalarCentering, y: ScalarCentering, z: ScalarCentering):
         self._comp_centerings = [x, y, z]
 
-    def comp_is_ccs(self, d: int) -> Bool3:
-        return self._comp_centerings[d].is_ccs
-
-    def comp_offsets(self, d: int) -> Float3:
-        return self._comp_centerings[d].offsets
-
     def copy(self) -> Self:
         return VectorCentering(*(comp_centering.copy() for comp_centering in self._comp_centerings))
+
+    def __getitem__(self, d: int) -> ScalarCentering:
+        return self._comp_centerings[d]
+
+    def __iter__(self):
+        yield from self._comp_centerings
 
     @classmethod
     def nc(cls) -> Self:
@@ -59,5 +59,5 @@ class VectorCentering:
 
 
 def test():
-    assert (VectorCentering.ec().comp_is_ccs(0) == Bool3(True, False, False)).all()
-    assert (VectorCentering.fc().comp_is_ccs(0) == Bool3(False, True, True)).all()
+    assert (VectorCentering.ec()[0].is_ccs == Bool3(True, False, False)).all()
+    assert (VectorCentering.fc()[0].is_ccs == Bool3(False, True, True)).all()
