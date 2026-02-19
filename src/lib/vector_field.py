bd777e7f377211ac99d09013714adfa02f0a474f
James McClung <james@themcclungs.net>
2026-02-19 08:32:58 -0500

    vector_field: +set_component_from_func

--- src/lib/vector_field.py
+++ src/lib/vector_field.py
@@ -1,6 +1,9 @@
+from typing import Callable
+
 from lib.centering import VectorCentering
 from lib.domain import Domain
 from lib.field_array import FieldArray
+from lib.range3 import Range3
 from lib.vec3 import Float3, Int3
 
 
@@ -18,6 +21,11 @@ class VectorField:
             # nc values at upper edge are in domain
             comp_dims = domain.dims + centering.component_centered(d).flip().to_mask() * domain.vary_dims.to_mask()
             self._components.append(FieldArray(comp_dims, n_ghosts=domain.vary_dims.to_mask() * domain.n_ghosts))
+
+    def set_component_from_func(self, d: int, func: Callable[[Float3], float]):
+        for i3 in Range3(self[d].dims):
+            pos = self.domain.corner_pos + self.domain.deltas * (i3.to_float3() + self.centering.component_offsets(d))
+            self[d][i3] = func(pos)
 
     def __getitem__(self, d: int) -> FieldArray:
         return self._components[d]
@@ -51,3 +59,11 @@ def test():
     assert (ec_field.z.dims == Int3(1, 9, 4)).all()
     assert (ec_field.z.n_ghosts_lower == Int3(0, 1, 1)).all()
     assert (ec_field.z.n_ghosts_upper == Int3(0, 1, 1)).all()
+
+    ec_field.set_component_from_func(0, lambda pos: pos.x)
+    ec_field.set_component_from_func(1, lambda pos: pos.x)
+    ec_field.set_component_from_func(2, lambda pos: pos.x)
+
+    assert ec_field.x[Int3(0, 1, 0)] == 0.25
+    assert ec_field.y[Int3(0, 1, 0)] == 0.0
+    assert ec_field.z[Int3(0, 1, 0)] == 0.0
