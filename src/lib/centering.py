5381fada21c29819287a9e07a0e4f14a359bf046
James McClung <james@themcclungs.net>
2026-02-19 09:27:42 -0500

    centering: +ScalarCentering

--- src/lib/centering.py
+++ src/lib/centering.py
@@ -3,50 +3,66 @@ from typing import Self
 from lib.vec3 import Bool3, Float3
 
 
-class VectorCentering:
-    def __init__(self, x_cc: Bool3, y_cc: Bool3, z_cc: Bool3):
-        self._is_ccss = [x_cc, y_cc, z_cc]
-        self._offsetss = [comp_is_ccs.to_mask().to_float3() * 0.5 for comp_is_ccs in self._is_ccss]
-
-    def comp_is_ccs(self, d: int) -> Bool3:
-        return self._is_ccss[d]
-
-    def comp_offsets(self, d: int) -> Float3:
-        return self._offsetss[d]
+class ScalarCentering:
+    def __init__(self, is_ccs: Bool3):
+        self.is_ccs = is_ccs
+        self.offsets = is_ccs.to_mask().to_float3() * 0.5
 
     def copy(self) -> Self:
-        return VectorCentering(*(is_comp_cc.copy() for is_comp_cc in self._is_ccss))
+        return ScalarCentering(self.is_ccs.copy())
+
+    @classmethod
+    def nc(cls) -> Self:
+        return cls(Bool3(False, False, False))
+
+    @classmethod
+    def cc(cls) -> Self:
+        return cls(Bool3(True, True, True))
+
+
+class VectorCentering:
+    def __init__(self, x: ScalarCentering, y: ScalarCentering, z: ScalarCentering):
+        self._comp_centerings = [x, y, z]
+
+    def comp_is_ccs(self, d: int) -> Bool3:
+        return self._comp_centerings[d].is_ccs
+
+    def comp_offsets(self, d: int) -> Float3:
+        return self._comp_centerings[d].offsets
+
+    def copy(self) -> Self:
+        return VectorCentering(*(comp_centering.copy() for comp_centering in self._comp_centerings))
 
     @classmethod
     def nc(cls) -> Self:
         return cls(
-            Bool3(False, False, False),
-            Bool3(False, False, False),
-            Bool3(False, False, False),
+            ScalarCentering.nc(),
+            ScalarCentering.nc(),
+            ScalarCentering.nc(),
         )
 
     @classmethod
     def ec(cls) -> Self:
         return cls(
-            Bool3(True, False, False),
-            Bool3(False, True, False),
-            Bool3(False, False, True),
+            ScalarCentering(Bool3(True, False, False)),
+            ScalarCentering(Bool3(False, True, False)),
+            ScalarCentering(Bool3(False, False, True)),
         )
 
     @classmethod
     def fc(cls) -> Self:
         return cls(
-            Bool3(False, True, True),
-            Bool3(True, False, True),
-            Bool3(True, True, False),
+            ScalarCentering(Bool3(False, True, True)),
+            ScalarCentering(Bool3(True, False, True)),
+            ScalarCentering(Bool3(True, True, False)),
         )
 
     @classmethod
     def cc(cls) -> Self:
         return cls(
-            Bool3(True, True, True),
-            Bool3(True, True, True),
-            Bool3(True, True, True),
+            ScalarCentering.cc(),
+            ScalarCentering.cc(),
+            ScalarCentering.cc(),
         )
 
 
