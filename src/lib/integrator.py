555b4627035d8a4272eb5a1bb212c8346248f482
James McClung <james@themcclungs.net>
2026-02-21 14:28:28 -0500

    integrator: +IntegratorBuilder

--- src/lib/integrator.py
+++ src/lib/integrator.py
@@ -1,12 +1,31 @@
+from __future__ import annotations
+
+from lib.domain import Domain
+from lib.ghost_manager import GhostManager3d
+from lib.ghost_setters.periodic import SetGhostsPeriodic
 from lib.push_fields import push_b, push_e
 from lib.state import State
 
 
-class Integrator:
-    def __init__(self, state: State, dt: float):
-        self.timestep = 0
+class IntegratorBuilder:
+    def __init__(self, domain: Domain, dt: float):
         self.dt = dt
-        self.state = state
+        self.state = State(domain)
+        self.ghost_manager = GhostManager3d.periodic()
+
+    def build(self) -> Integrator:
+        return Integrator(self)
+
+
+class Integrator:
+    def __init__(self, builder: IntegratorBuilder):
+        self.state = builder.state
+        self.dt = builder.dt
+        self.timestep = 0
+        self.ghost_manager = builder.ghost_manager
+        for d in range(3):
+            if self.state.domain.periodic_dims[d]:
+                assert isinstance(self.ghost_manager._managers[d], SetGhostsPeriodic)
 
     def integrate(self, n_steps: int | None = None):
         if n_steps is None:
