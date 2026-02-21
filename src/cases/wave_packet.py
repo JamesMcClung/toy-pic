5c3d7912ca1b50205d534df526528ae8f1a490fb
James McClung <james@themcclungs.net>
2026-02-21 15:17:20 -0500

    +cases/wave_packet

---
+++ src/cases/wave_packet.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from cases.case_base import Case
+from lib.domain import Domain
+from lib.vec3 import Float3
+
+
+class WavePacket(Case):
+    def __init__(self, domain: Domain):
+        self.domain = domain
+
+        self.packet_width = domain.lengths.x / 16.0
+        self.packet_pos = domain.corner_pos.x + self.packet_width / 2.0
+
+    def ey(self, pos: Float3) -> float:
+        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2))
+
+    def by(self, pos: Float3) -> float:
+        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2))
