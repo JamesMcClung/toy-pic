6289c9996ac9acb615452ac161c175cb8e2dc959
James McClung <james@themcclungs.net>
2026-02-21 14:38:19 -0500

    domain: rename to n_cells

--- src/lib/domain.py
+++ src/lib/domain.py
@@ -6,18 +6,18 @@ from lib.vec3 import Bool3, Float3, Int3
 class Domain:
     def __init__(
         self,
-        dims: Int3,
+        n_cells: Int3,
         deltas: Float3,
         *,
         corner_pos: Float3 | None = None,
         periodic_dims: Bool3 | None = None,
     ):
-        self.dims = dims
+        self.dims = n_cells
         self.deltas = deltas
         self.corner_pos = corner_pos or Float3(0, 0, 0)
         self.periodic_dims = periodic_dims or Bool3(True, True, True)
 
-        self.vary_dims = ~((dims == Int3(1, 1, 1)) & self.periodic_dims)
+        self.vary_dims = ~((n_cells == Int3(1, 1, 1)) & self.periodic_dims)
         self.lengths = self.dims.to_float3() * self.deltas
 
     def __eq__(self, other: Any) -> bool:
