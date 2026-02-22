2cd34549ddf56d486ec6860d920e1693bec8ee63
James McClung <james@themcclungs.net>
2026-02-21 20:36:01 -0500

    wave_packet: add actual waves in packet

--- src/cases/wave_packet.py
+++ src/cases/wave_packet.py
@@ -11,9 +11,10 @@ class WavePacket(Case):
 
         self.packet_width = domain.lengths.x / 16.0
         self.packet_pos = domain.corner_pos.x + self.packet_width / 2.0
+        self.wavelength = self.packet_width / 2.0
 
     def ey(self, pos: Float3) -> float:
-        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2))
+        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2)) * np.sin(2.0 * np.pi * (pos.x - self.packet_pos) / self.wavelength)
 
     def by(self, pos: Float3) -> float:
-        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2))
+        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2)) * np.sin(2.0 * np.pi * (pos.x - self.packet_pos) / self.wavelength)
