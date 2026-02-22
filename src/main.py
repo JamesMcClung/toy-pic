75136f439d77e10a1f842397b5dcf9c81fff2fd3
James McClung <james@themcclungs.net>
2026-02-22 13:24:40 -0500

    main: bigger domain

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from lib.integrator import IntegratorBuilder
 from lib.state import State
 from lib.vec3 import Float3, Int3
 
-domain = Domain(Int3(64, 4, 4), Float3(1.0, 1.0, 1.0))
+domain = Domain(Int3(256, 2, 1), Float3(1.0, 1.0, 1.0))
 
 builder = IntegratorBuilder(domain, 0.15)
 
