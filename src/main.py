4d494539574653d1e5eb3fa1d789e64584f299ec
James McClung <james@themcclungs.net>
2026-02-21 20:15:14 -0500

    main: render e with fpl

--- src/main.py
+++ src/main.py
@@ -1,14 +1,37 @@
+import fastplotlib as fpl
+import numpy as np
+
 from cases.wave_packet import WavePacket
 from lib.domain import Domain
 from lib.integrator import IntegratorBuilder
+from lib.state import State
 from lib.vec3 import Float3, Int3
 
 domain = Domain(Int3(64, 4, 4), Float3(1.0, 1.0, 1.0))
 
-builder = IntegratorBuilder(domain, 0.75)
+builder = IntegratorBuilder(domain, 0.15)
 
 WavePacket(domain).init_state(builder.initial_state)
 
 integrator = builder.build()
 
-integrator.integrate(10)
+figure = fpl.Figure(size=(700, 560))
+
+
+def get_image_data(state: State) -> np.ndarray:
+    return state.e.y._inner_array.mean(axis=2).T
+
+
+IMAGE_NAME = "image"
+figure[0, 0].add_image(data=get_image_data(integrator.state), name=IMAGE_NAME)
+
+
+def next_image(subplot: fpl.layouts.Subplot):
+    integrator.step()
+    subplot[IMAGE_NAME].data = get_image_data(integrator.state)
+
+
+figure[0, 0].add_animations(next_image)
+
+figure.show(maintain_aspect=False)
+fpl.loop.run()
