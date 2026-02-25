ad5c7fc0ca60f10f0eccecf4b1a087e54d4cc0c4
James McClung <james@themcclungs.net>
2026-02-25 07:25:45 -0500

    wave_packet: factor of 1/2 in envelope

--- src/lib/initial_conditions/wave_packet.py
+++ src/lib/initial_conditions/wave_packet.py
@@ -21,11 +21,11 @@ class WavePacket(InitialCondition):
 
         self.packet_width = width if width is not None else domain.lengths.x / 16.0
         self.packet_pos = pos if pos is not None else domain.corner_pos.x + self.packet_width / 2.0
-        self.wavelength = wavelength if wavelength is not None else self.packet_width / 2.0
+        self.wavelength = wavelength if wavelength is not None else self.packet_width
         self.dir = float(dir)
 
     def ey(self, pos: Float3) -> float:
-        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2)) * np.sin(2.0 * np.pi * (pos.x - self.packet_pos) / self.wavelength)
+        return 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2) / 2.0) * np.sin(2.0 * np.pi * (pos.x - self.packet_pos) / self.wavelength)
 
     def bz(self, pos: Float3) -> float:
-        return self.dir * 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2)) * np.sin(2.0 * np.pi * (pos.x - self.packet_pos) / self.wavelength)
+        return self.dir * 1.0 * np.exp(-(((pos.x - self.packet_pos) / self.packet_width) ** 2) / 2.0) * np.sin(2.0 * np.pi * (pos.x - self.packet_pos) / self.wavelength)
